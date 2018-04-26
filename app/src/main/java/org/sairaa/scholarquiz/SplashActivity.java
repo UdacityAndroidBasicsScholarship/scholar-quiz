38bdc4e190bba3bc11a7b0d99532fdc3b9a758d1
Gagan Kumar Indoriya <gaganindoriya0@gmail.com>
2018-04-26 07:05:41 -0700

    Added splash screen (#35)
    1-changed SplashActivity.java
    2-changed activity_splash.xml
    3-added two images in drawable dir(backgroundwall3d.png & splashlogo3d.ppng)
    4-added two new files for animation in > anim dir under > res dir

--- app/src/main/java/org/sairaa/scholarquiz/SplashActivity.java
+++ app/src/main/java/org/sairaa/scholarquiz/SplashActivity.java
@@ -3,39 +3,81 @@ package org.sairaa.scholarquiz;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.PixelFormat;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import org.sairaa.scholarquiz.ui.Login.LoginActivity;
 
 public class SplashActivity extends Activity {
 
-    // Splash screen timer
-    private static int SPLASH_TIME_OUT = 3000;
+    Thread splashThread;
+    private static int SPLASH_TIME_OUT = 3500;
+
+
+    public void onAttachedToWindow(){
+        super.onAttachedToWindow();
+        Window window=getWindow();
+        window.setFormat(PixelFormat.RGBA_8888);
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
-        new Handler().postDelayed(new Runnable() {
-
-            /*
-             * Showing splash screen with a timer. This will be useful when you
-             * want to show case your app logo / company
-             */
-
-            @Override
-            public void run() {
-                // This method will be executed once the timer is over
-                // Start your app main activity
-                Intent i = new Intent(SplashActivity.this, LoginActivity.class);
-                startActivity(i);
-
-                // close this activity
-                finish();
-            }
-        }, SPLASH_TIME_OUT);
+        StartAnimation();
     }
+    private void StartAnimation(){
+        Animation anim = AnimationUtils.loadAnimation(this,R.anim.alpha);
+        anim.reset();
+        LinearLayout l=(LinearLayout)findViewById(R.id.lin_lay);
+        l.clearAnimation();
+        l.startAnimation(anim);
+        anim=AnimationUtils.loadAnimation(this,R.anim.translate);
+        anim.reset();
+        TextView tv=(TextView) findViewById(R.id.splashtagline);
+        tv.clearAnimation();
+        tv.startAnimation(anim);
+        TextView tvDev=(TextView) findViewById(R.id.splashDeveloper);
+        tvDev.clearAnimation();
+        tvDev.startAnimation(anim);
 
+        splashThread= new  Thread(){
+            public void  run(){
+                try{
+                    int waited=0;
+                    //splash screen pause time
+                    while(waited<SPLASH_TIME_OUT){
+                        sleep(100);
+                        waited+=100;
+                    }
+                    Intent intent= new Intent(SplashActivity.this,LoginActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                    startActivity(intent);
+                    SplashActivity.this.finish();
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                finally {
+                    SplashActivity.this.finish();
+                }
+            }
+        };
+        splashThread.start();
+
+
+    }
 }
+
+
+
+
